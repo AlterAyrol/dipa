a2d0812e708ad2f2d01d940401f86a75b021522d
AlterAyrol <lazarev.reach@gmail.com>
2023-09-12 17:56:01 +0300

    change browser start configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,26 +14,6 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(autouse=True)
-def web_browser():
-    # driver_options = webdriver.ChromeOptions()
-    # driver_options.add_argument('--headless')
-    # browser.config.driver_options = driver_options
-    browser.config.window_width = 1400
-    browser.config.window_height = 1600
-    browser.config.base_url = 'https://dipa.tech/'
-
-    yield
-
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
-
-    browser.quit()
-
-
-
 # @pytest.fixture(autouse=True)
 # def web_browser():
 #     # driver_options = webdriver.ChromeOptions()
@@ -42,25 +22,6 @@ def web_browser():
 #     browser.config.window_width = 1400
 #     browser.config.window_height = 1600
 #     browser.config.base_url = 'https://dipa.tech/'
-#     options = Options()
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": "100.0",
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True
-#         }
-#     }
-#
-#     login = os.getenv('LOGIN')
-#     password = os.getenv('PASSWORD')
-#
-#     options.capabilities.update(selenoid_capabilities)
-#     driver = webdriver.Remote(
-#         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-#         options=options)
-#
-#     browser.config.driver = driver
 #
 #     yield
 #
@@ -70,4 +31,42 @@ def web_browser():
 #     attach.add_video(browser)
 #
 #     browser.quit()
+
+
+@pytest.fixture(autouse=True)
+def web_browser():
+    # driver_options = webdriver.ChromeOptions()
+    # driver_options.add_argument('--headless')
+    # browser.config.driver_options = driver_options
+    browser.config.window_width = 1400
+    browser.config.window_height = 1600
+    browser.config.base_url = 'https://dipa.tech/'
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
+    yield
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+
+    browser.quit()
 
